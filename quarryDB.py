36be71a12e91e25a31ba4b62099453fcfaea8896
lelecha <maguoqi96@gmail.com>
2020-07-03 14:54:20 +0800

    “finish curd”

--- quarryDB.py
+++ quarryDB.py
@@ -82,68 +82,248 @@ def quarry_all(tablename):
 
 
 # 测试版 最后加入所有条件
-def quarry_search(name, bit, numK):
-    # dic = {
-    #
-    #     name: 0,
-    #     bit: 1,
-    #     numK: 2,
-    #
-    # }
-    list = [None] * 3  # 后期会改
-    if bit == '':
-        list[1] = '1'
-        bit = '1'
-    else:
-        list[1] = 'bit'
+# def quarry_search(name, bit, numK):
+#     # dic = {
+#     #
+#     #     name: 0,
+#     #     bit: 1,
+#     #     numK: 2,
+#     #
+#     # }
+#     list = [None] * 3  # 后期会改
+#     if bit == '':
+#         list[1] = '1'
+#         bit = '1'
+#     else:
+#         list[1] = 'bit'
+#
+#     if numK == '':
+#         list[2] = '1'
+#         numK = '1'
+#     else:
+#         list[2] = 'numK'
+#
+#     if name == '':
+#         list[0] = '1'
+#         name = '1'
+#     else:
+#         list[0] = 'name'
+#     print(list)
+#     #还要遍历所有table
+#     sql_quarry = "select * from {tablename} where {name} = {name_} " \
+#                  "and {bit} = {bit_} " \
+#                  "and {numK} = {numK_};".format(tablename = 'test',
+#                                                  name = list[0],
+#                                                  name_= name,
+#                                                  bit = list[1],
+#                                                  bit_= bit,
+#                                                  numK = list[2],
+#                                                  numK_= numK
+#                                                )
+#     print(sql_quarry)
+#     db = sqlite3.connect('../test2.db')
+#     cur = db.cursor()
+#     cur.execute(sql_quarry)
+#     results = cur.fetchall()
+#     print(results)
+#     return results
 
-    if numK == '':
-        list[2] = '1'
-        numK = '1'
-    else:
-        list[2] = 'numK'
+def quarry_search(tablename, algo, name, bit, numK
+                  , tidW, tidN, subTidW, subTidN, sTypeW_bit, sType, storeLocation, algoId, ISSU, algoSpe,
+                  testSpe, castType, iOrd,
+                  TBLM_ID, dpt, dpt_person, confirmation):
+    list = [None] * 21
 
-    if name == '':
+    if algo == 'N/A' or algo == '':
         list[0] = '1'
+        algo = '1'
+    else:
+        list[0] = 'algo'
+
+    if name == 'N/A' or name == '':
+        list[1] = '1'
         name = '1'
     else:
-        list[0] = 'name'
-    print(list)
-    #还要遍历所有table
-    sql_quarry = "select * from {tablename} where {name} = {name_} " \
-                 "and {bit} = {bit_} " \
-                 "and {numK} = {numK_};".format(tablename = 'test',
-                                                 name = list[0],
-                                                 name_= name,
-                                                 bit = list[1],
-                                                 bit_= bit,
-                                                 numK = list[2],
-                                                 numK_= numK
-                                               )
-    print(sql_quarry)
+        list[1] = 'name'
+
+    if bit == 'N/A' or bit == '':
+        list[2] = '1'
+        bit = '1'
+    else:
+        list[2] = 'bit'
+
+    if numK == 'N/A' or numK == '':
+        list[3] = '1'
+        numK = '1'
+    else:
+        list[3] = 'numK'
+
+    if tidW == 'N/A' or tidW == '':
+        list[4] = '1'
+        tidW = '1'
+    else:
+        list[4] = 'tidW'
+
+    if tidN == 'N/A' or tidN == '':
+        list[5] = '1'
+        tidN = '1'
+    else:
+        list[5] = 'tidN'
+
+    if subTidW == 'N/A' or subTidW == '':
+        list[6] = '1'
+        subTidW = '1'
+    else:
+        list[6] = 'subTidW'
+
+    if subTidN == 'N/A' or subTidN == '':
+        list[7] = '1'
+        subTidN = '1'
+    else:
+        list[7] = 'subTidN'
+
+    if sTypeW_bit == 'N/A' or sTypeW_bit == '':
+        list[8] = '1'
+        sTypeW_bit = '1'
+    else:
+        list[8] = 'sTypeW_bit'
+
+    if sType == 'N/A' or sType == '':
+        list[9] = '1'
+        sType = '1'
+    else:
+        list[9] = 'sType'
+
+    if storeLocation == 'N/A' or storeLocation == '':
+        list[10] = '1'
+        storeLocation = '1'
+    else:
+        list[10] = 'storeLocation'
+
+    if algoId == 'N/A' or algoId == '':
+        list[11] = '1'
+        algoId = '1'
+    else:
+        list[11] = 'algoId'
+
+    if ISSU == 'N/A' or ISSU == '':
+        list[12] = '1'
+        ISSU = '1'
+    else:
+        list[12] = 'ISSU'
+
+
+    if algoSpe == 'N/A' or algoSpe == '':
+        list[13] = '1'
+        algoSpe = '1'
+    else:
+        list[13] = 'algoSpe'
+
+    if testSpe == 'N/A' or testSpe == '':
+        list[14] = '1'
+        testSpe = '1'
+    else:
+        list[14] = 'testSpe'
+
+    if castType == 'N/A' or castType == '':
+        list[15] = '1'
+        castType = '1'
+    else:
+        list[15] = 'castType'
+
+    if iOrd == 'N/A' or iOrd == '':
+        list[16] = '1'
+        iOrd = '1'
+    else:
+        list[16] = 'iOrd'
+
+    if TBLM_ID == 'N/A' or TBLM_ID == '':
+        list[17] = '1'
+        TBLM_ID = '1'
+    else:
+        list[17] = 'TBLM_ID'
+
+    if dpt == 'N/A' or dpt == '':
+        list[18] = '1'
+        dpt = '1'
+    else:
+        list[18] = 'dpt'
+
+    if dpt_person == 'N/A' or dpt_person == '':
+        list[19] = '1'
+        dpt_person = '1'
+    else:
+        list[19] = 'dpt_person'
+
+    if confirmation == 'N/A' or confirmation == '':
+        list[20] = '1'
+        confirmation = '1'
+    else:
+        list[20] = 'confirmation'
+
+
+    sql_search = "select * from '{tablename}' where {algo} = {algo_} and {name} = {name_} and {bit} = {bit_}" \
+                 " and {numK} = {numK_} and {tidW} = {tidW_} and {tidN} = {tidN_} and {subTidW} = {subTidW_}" \
+                 " and {subTidN} = {subTidN_} and {sTypeW_bit} = {sTypeW_bit_} and {sType} = {sType_} and {storeLocation} = {storeLocation_}" \
+                 " and {algoId} = {algoId_} and {ISSU} = {ISSU_} and {algoSpe} = {algoSpe_} and {testSpe} = {testSpe_}" \
+                 " and {castType} = {castType_} and {iOrd} = {iOrd_} and {TBLM_ID} = {TBLM_ID_} and {dpt} = {dpt_}" \
+                 " and {dpt_person} = {dpt_person_} and {confirmation} = {confirmation_}".format(tablename=tablename,
+                                                                                                 algo=list[0],
+                                                                                                 algo_=algo,
+                                                                                                 name=list[1],
+                                                                                                 name_=name,
+                                                                                                 bit=list[2],
+                                                                                                 bit_=bit,
+                                                                                                 numK=list[3],
+                                                                                                 numK_=numK,
+                                                                                                 tidW=list[4],
+                                                                                                 tidW_=tidW,
+                                                                                                 tidN=list[5],
+                                                                                                 tidN_=tidN,
+                                                                                                 subTidW=list[6],
+                                                                                                 subTidW_=subTidW,
+                                                                                                 subTidN=list[7],
+                                                                                                 subTidN_=subTidN,
+                                                                                                 sTypeW_bit=list[8],
+                                                                                                 sTypeW_bit_=sTypeW_bit,
+                                                                                                 sType=list[9],
+                                                                                                 sType_=sType,
+                                                                                                 storeLocation=list[10],
+                                                                                                 storeLocation_=storeLocation,
+                                                                                                 algoId=list[11],
+                                                                                                 algoId_=algoId,
+                                                                                                 ISSU=list[12],
+                                                                                                 ISSU_=ISSU,
+                                                                                                 algoSpe=list[13],
+                                                                                                 algoSpe_=algoSpe,
+                                                                                                 testSpe=list[14],
+                                                                                                 testSpe_=testSpe,
+                                                                                                 castType=list[15],
+                                                                                                 castType_=castType,
+                                                                                                 iOrd=list[16],
+                                                                                                 iOrd_=iOrd,
+                                                                                                 TBLM_ID=list[17],
+                                                                                                 TBLM_ID_=TBLM_ID,
+                                                                                                 dpt=list[18],
+                                                                                                 dpt_=dpt,
+                                                                                                 dpt_person=list[19],
+                                                                                                 dpt_person_=dpt_person,
+                                                                                                 confirmation=list[20],
+                                                                                                 confirmation_=confirmation
+                                                                                                 )
+    print(sql_search)
     db = sqlite3.connect('../test2.db')
-    cur = db.cursor()
-    cur.execute(sql_quarry)
-    results = cur.fetchall()
-    print(results)
-    return results
-    # sql_quarry = "select * from {tablename} where ;".format(tablename = tablename)
-    # try:
-    #     db = sqlite3.connect('test2.db')
-    #     # db = connectDB.connect_db('localhost', 'root', 'mgq960830!', '5885ce76mb')
-    #     cur = db.cursor()
-    #     cur.execute(sql_quarry)
-    #     headName = quarry_field_name(cur)
-    #     results = cur.fetchall()
-    #     results.insert(0, headName)
-    #     print(results)
-    #     # for row in results:
-    #     #     id = row[0]
-    #     #     user_id = row[1]
-    #     #     name = row[2]
-    #     #     print(id, name, user_id)
-    #     return results
+    cur_search = db.cursor()
+    cur_search.execute(sql_search)
+    results = cur_search.fetchall()
+    print(cur_search.fetchall())
+    db.commit()
+
+    print('success search')
     # except Exception as e:
-    #     raise e
+    #     db.rollback()
+    #     print('rollback')
     # finally:
-    #     db.close()
+    db.close()
+    return results
+
